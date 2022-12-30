568a15df425cbf1f1878a4305489b58581b41d84
chessboard092834 <121668711+chessboard092834@users.noreply.github.com>
2022-12-30 16:43:50 +0530

    - change config to something that suits

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://domain";
+export const BASE_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://chessboard-backend-nest.onrender.com";
