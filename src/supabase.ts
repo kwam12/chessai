52c0c01d16f4efb21aef54e9c5e66cda9632c337
chessboard092834 <121668711+chessboard092834@users.noreply.github.com>
2022-12-30 16:12:40 +0530

    - change supabase credentials

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://slkrvdtrtoltpaacecfr.supabase.co";
+const supabaseUrl = "https://vncokpktgnffojvcmzva.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsa3J2ZHRydG9sdHBhYWNlY2ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAwOTU4NzEsImV4cCI6MTk4NTY3MTg3MX0.phcGdTW19YJKD86_gf3VE6SRZ5ohN7cEW5b94waO89U";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuY29rcGt0Z25mZm9qdmNtenZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIzOTU4MjgsImV4cCI6MTk4Nzk3MTgyOH0.uLsV3ONqSWru7G35-mLmwI3Bkzv1xSXrjIYF5V875xc";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
